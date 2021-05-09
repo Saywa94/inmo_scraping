5a18df2d53f04791f6f808be5c2451d83246a3bd
Saywa Chavez <saywachavez@gmail.com>
2021-05-09 12:57:54 -0400

    Improved code, better separation of concerns

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
 import time
-
 from utils import zoneTypeOfferSeparator, getPropertiesInPageData, writeToJson
 
 
-selectors = {
-    'container_css_selector':   '.inmuebles-item:not(.ads_LISTADO_INMUEBLESPos1)',
-    'price_selector':           '.inmuebles-item-precio h4',
-    'title_selector':           'h2.text-ellipsis.line-height-30px',
-    'zone_type_offer_selector': 'h3.text-ellipsis',
-    'bed_wc_surface_selector':  'ul.list-inline li.icon-default-color',
-}
+# PARAMETROS
+# #############################################
+container_css_selector = '.inmuebles-item:not(.ads_LISTADO_INMUEBLESPos1)'
+price_selector = '.inmuebles-item-precio h4'
+title_selector = 'h2.text-ellipsis.line-height-30px'
+zone_type_offer_selector = 'h3.text-ellipsis'
+bed_wc_surface_selector = 'ul.list-inline li.icon-default-color'
 
 output_file = 'output/casas_data.json'
-all_data = []
+
+max_pages = 3
+# ##############################################
+
+selectors = (
+    container_css_selector,
+    price_selector,
+    title_selector,
+    zone_type_offer_selector,
+    bed_wc_surface_selector
+)
 
 page_number = 1
-# Para casas en venta en La Paz, solo hay 41 paginas
-max_pages = 3
+all_data = []
 
-    
 tic = time.perf_counter()
 while True:
     if page_number == max_pages + 1:
-        print('Max number of pages attained')
         break
     try:
         page_data = getPropertiesInPageData(page_number, selectors)
@@ -32,6 +38,7 @@ while True:
     all_data.extend(page_data)
     page_number += 1
 toc = time.perf_counter()
+
 
 thic = time.perf_counter()
 writeToJson(output_file, all_data)
